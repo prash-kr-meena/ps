3b0eb43796529b6c29c25f6f28b2894f1575bb29
prashant_meena <prashantme@zeta.tech>
2022-08-28 01:31:56 +0530

    count_digits

---
+++ 1_Basics/4_Math/count_digits.py
@@ -0,0 +1,54 @@
+"""
+Count digits in a number
+https://takeuforward.org/data-structure/count-digits-in-a-number/
+
+
+Problem Statement: Given an integer N , write program to count number of digits in N.
+
+Examples:
+    Input: N = 12345
+    Output: 5
+
+    Input: N = 8394
+    Output: 4
+
+
+
+Good Edge Cases
+* Negative Number (Here the -ve sign should not be counted)
+* Zero
+"""
+import math
+
+
+def count_digits(num: int) -> int:
+    """
+    Time Complexity: O (n) where n is the number of digits in the given integer
+    Space Complexity: O(1)
+    """
+    num = abs(num)  # Edge Cases, to handle negative values
+
+    if num == 0:  # Edge case
+        return 1
+
+    count = 0
+    while num > 0:
+        count += 1
+        num = num // 10
+    return count
+
+
+def count_digits_string(num: int) -> int:
+    """
+    Time Complexity: O (1)
+    Space Complexity: O(1)
+    """
+    num = abs(num)  # Handling -ve cases
+    number_string = str(num)
+    return len(number_string)
+
+
+if __name__ == '__main__':
+    numbers = [3, 0, -1, -144, 435345345]
+    for number in numbers:
+        print(count_digits(num=number), count_digits_string(num=number))
